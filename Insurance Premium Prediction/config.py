e6dc2cf09bcf683911a82563e78c0e21436e6c4b
Motheeshkumar J <motheesh96@gmail.com>
2021-08-27 14:22:51 +0530

    update

--- Insurance Premium Prediction/config.py
+++ Insurance Premium Prediction/config.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 CLIENT_ID='eMPpqbdGanGiwoPwtyZqSFDu'
 SECRET_KEY='arFTsjAscGIj5i6dgH2oEN_uIzXdtO-.se8zE_U91bfDDs0lx3QnjU64gF-WYFceBG8EF6Z6c7plG4Zmeesb2,.Pn3X2kHz5FoZG6Fk.JiZKsbOG0.W9xyKNpklbYFMx'
 BUNDLE_PATH='.\\DBbundle\\secure-connect-mlproject.zip'
